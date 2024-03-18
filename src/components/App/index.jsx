829e504ae8626a496c05b4606e8d424ac80c095f
GolyaevaEkaterina <katrin.alien1@gmail.com>
2024-03-18 16:59:10 +0300

    запросы

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,23 +3,41 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [type, setType] = useState("loading")
-  // const [items, setItems] = useState([])
+  const [type, setType] = useState("posts")
+  const [items, setItems] = useState([])
+  
   useEffect(()=>{
     console.log("mount")
-  },[])
+    fetch(`https://gorest.co.in/public/v2/${type}`)
+    .then(data => data.json())
+    .then(res => setItems(res))
+  }, [])
+
   useEffect(()=>{
-    console.log("update")
-  })
+    console.log("update type:", type )
+    fetch(`https://gorest.co.in/public/v2/${type}`)
+    .then(data => data.json())
+    .then(res => setItems(res))
+  }, [type])
+
+  useEffect(()=>{
+    console.log("update items" )
+    localStorage.setItem("items", JSON.stringify(items))
+  }, [items])
+
   return (
     <div>
         <div className="App">
-            <Button handleClick={()=>setType("loading")} title="загрузка"/>
-            <Button handleClick={()=>setType("update")} title="обновление"/>
-            <Button handleClick={()=>setType("deletion")} title="удаление" />
+            <Button handleClick={()=>setType("users")} title="Пользователи"/>
+            <Button handleClick={()=>setType("posts")} title="Посты"/>
+            <Button handleClick={()=>setType("comments")} title="Комментарии" />
         </div>
 
         <div className='text-4xl flex justify-center'>{type}</div>
+
+        {items.map((item) => (
+          <pre key={item.id}>{JSON.stringify(item)}</pre>
+        ))}
     </div>    
   );
 }
